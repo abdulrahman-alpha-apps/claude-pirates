84a7383b02b95b700fc44b67dccf4df20258bf5c
abdulrahman-alpha-apps <abdulrahman.Alhalabi@alpha-apps.ae>
2026-04-14 15:44:46 +0300

    Set correct baseUrl and site title for Claude Pirates

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Claude Pirates",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "abdulrahman-alpha-apps.github.io/claude-pirates",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
